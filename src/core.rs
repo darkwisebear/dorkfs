8bd718e1f66c2e084bb2913205f94c6750667140
Markus Hosch <markus.hosch@gmx.net>
2018-02-18 01:47:52 +0100

    Implement getattr for "/"

--- src/core.rs
+++ src/core.rs
@@ -3,6 +3,7 @@ use failure::Error;
 use std::result::Result;
 use fuse_mt::*;
 use libc;
+use time::get_time;
 
 pub struct DorkFs;
 
@@ -11,8 +12,28 @@ impl FilesystemMT for DorkFs {
         Ok(())
     }
 
-    fn getattr(&self, _req: RequestInfo, _path: &Path, _fh: Option<u64>) -> ResultEntry {
-        Err(libc::ENOSYS)
+    fn getattr(&self, _req: RequestInfo, path: &Path, _fh: Option<u64>) -> ResultEntry {
+        if path.as_os_str() == "/" {
+            let attr = FileAttr {
+                size: 4096,
+                blocks: 1,
+                atime: get_time(),
+                mtime: get_time(),
+                ctime: get_time(),
+                crtime: get_time(),
+                kind: FileType::Directory,
+                perm: (7 << 6) + (5 << 3) + 0,
+                nlink: 1,
+                uid: 1000,
+                gid: 1000,
+                rdev: 0,
+                flags: 0
+            };
+
+            Ok((get_time(), attr))
+        } else {
+            Err(libc::ENOENT)
+        }
     }
 
     fn opendir(&self, _req: RequestInfo, path: &Path, _flags: u32) -> ResultOpen {
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate clap;
 extern crate failure;
 extern crate fuse_mt;
 extern crate libc;
+extern crate time;
 #[macro_use] extern crate log;
 extern crate env_logger;
 
