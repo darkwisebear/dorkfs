10272a66b90dcf80c233b1f9d6a97c98d454e7f9
Markus Hosch <markus.hosch@gmx.net>
2018-02-18 02:22:41 +0100

    Add sample file in root

--- src/core.rs
+++ src/core.rs
@@ -1,4 +1,5 @@
 use std::path::Path;
+use std::ffi::OsString;
 use failure::Error;
 use std::result::Result;
 use fuse_mt::*;
@@ -13,7 +14,8 @@ impl FilesystemMT for DorkFs {
     }
 
     fn getattr(&self, _req: RequestInfo, path: &Path, _fh: Option<u64>) -> ResultEntry {
-        if path.as_os_str() == "/" {
+        let p = path.as_os_str();
+        if p == "/" {
             let attr = FileAttr {
                 size: 4096,
                 blocks: 1,
@@ -31,14 +33,56 @@ impl FilesystemMT for DorkFs {
             };
 
             Ok((get_time(), attr))
+        } else if p == "/test.txt" {
+            let attr = FileAttr {
+                size: 13,
+                blocks: 1,
+                atime: get_time(),
+                mtime: get_time(),
+                ctime: get_time(),
+                crtime: get_time(),
+                kind: FileType::RegularFile,
+                perm: (6 << 6) + (4 << 3) + 4,
+                nlink: 1,
+                uid: 1000,
+                gid: 1000,
+                rdev: 0,
+                flags: 0
+            };
+
+            Ok((get_time(), attr))
         } else {
             Err(libc::ENOENT)
         }
     }
 
-    fn opendir(&self, _req: RequestInfo, path: &Path, _flags: u32) -> ResultOpen {
-        info!("opendir on path {:?}", path);
-        Err(libc::ENOSYS)
+    fn opendir(&self, _req: RequestInfo, path: &Path, flags: u32) -> ResultOpen {
+        if path.as_os_str() == "/" {
+            Ok((17, flags))
+        } else {
+            Err(libc::ENOENT)
+        }
+    }
+
+    fn readdir(&self, _req: RequestInfo, path: &Path, _fh: u64) -> ResultReaddir {
+        if path.as_os_str() == "/" {
+            Ok(vec![
+                DirectoryEntry {
+                    name: OsString::from("."),
+                    kind: FileType::Directory
+                },
+                DirectoryEntry {
+                    name: OsString::from(".."),
+                    kind: FileType::Directory
+                },
+                DirectoryEntry {
+                    name: OsString::from("test.txt"),
+                    kind: FileType::RegularFile
+                }
+            ])
+        } else {
+            Err(libc::ENOENT)
+        }
     }
 }
 
