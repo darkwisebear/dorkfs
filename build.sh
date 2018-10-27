8839588c807e0ea7eaa2e691924c252d0c36aa4c
darkwisebear <markus.hosch@gmx.net>
2018-10-27 09:07:47 +0200

    Commit on top of the test branch

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -e
-
-cargo build --color always --features gitcache
-RUST_BACKTRACE=1 RUST_LOG=dorkfs=debug cargo test --verbose --color always --features gitcache -- --color always --nocapture
